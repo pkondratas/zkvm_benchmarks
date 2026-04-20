4f47a7637978d388d1fbc64af8476f268dddd30a
pkondratas <paulius.kondratas@gmail.com>
2026-04-20 22:33:31 +0300

    Fix file names

--- sp1_bench.sh
+++ sp1_bench.sh
@@ -5,9 +5,9 @@
 # SP1
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 1 --max-segment-limit 20 execute 2>&1 | tee -a sp1_1_sig.txt
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 1 --max-segment-limit 20 prove 2>&1 | tee -a sp1_1_sig.txt
-RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 10 --max-segment-limit 20 execute 2>&1 | tee -a sp1_1_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 10 --max-segment-limit 20 execute 2>&1 | tee -a sp1_10_sig.txt
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 10 --max-segment-limit 20 prove 2>&1 | tee -a sp1_10_sig.txt
-RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 50 --max-segment-limit 20 execute 2>&1 | tee -a sp1_1_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 50 --max-segment-limit 20 execute 2>&1 | tee -a sp1_50_sig.txt
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 50 --max-segment-limit 20 prove 2>&1 | tee -a sp1_50_sig.txt
-RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 100 --max-segment-limit 20 execute 2>&1 | tee -a sp1_1_sig.txt
-RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 100 --max-segment-limit 20 prove 2>&1 | tee -a sp1_50_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 100 --max-segment-limit 20 execute 2>&1 | tee -a sp1_100_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release --bin sp1-host -- --n-signatures 100 --max-segment-limit 20 prove 2>&1 | tee -a sp1_100_sig.txt
