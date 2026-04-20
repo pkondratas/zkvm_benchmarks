f23103ae6a97f38a633784f7ad007437db8017b6
pkondratas <paulius.kondratas@gmail.com>
2026-04-20 23:44:27 +0300

    Add gpu test case

--- risc0_bench.sh
+++ risc0_bench.sh
@@ -4,6 +4,7 @@
 
 # RISC0
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 1 --max-segment-limit 20 2>&1 | tee -a risc0_1_sig.txt
-# RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 10 --max-segment-limit 20 2>&1 | tee -a risc0_10_sig.txt
-# RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 50 --max-segment-limit 20 2>&1 | tee -a risc0_50_sig.txt
-# RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 100 --max-segment-limit 20 2>&1 | tee -a risc0_50_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 10 --max-segment-limit 20 2>&1 | tee -a risc0_10_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 20 --max-segment-limit 20 2>&1 | tee -a risc0_20_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 50 --max-segment-limit 20 2>&1 | tee -a risc0_50_sig.txt
+RUSTFLAGS="-C target-cpu=native" RUST_LOG=info RISC0_INFO=1 cargo run --release --bin risc0-host -- --n-signatures 100 --max-segment-limit 20 2>&1 | tee -a risc0_50_sig.txt
