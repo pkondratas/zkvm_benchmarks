93414a7d57e148b761a4b7f1e70d0ef7f7c5ac7e
pkondratas <paulius.kondratas@gmail.com>
2026-05-11 15:13:06 +0300

    Adjust scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "runpod_pod" "gpu_instance" {
   name              = "gpu-instance"
   image_name        = "pkondratas/zkvm-gpu-bench"
   compute_type      = "GPU"
-  gpu_type_ids      = ["NVIDIA RTX A4500"]  #["NVIDIA RTX PRO 4500 Blackwell"]
+  gpu_type_ids      = ["NVIDIA RTX PRO 4500 Blackwell"]
   data_center_ids   = ["EU-RO-1"]
 
   gpu_count            = 1
@@ -37,9 +37,9 @@ resource "runpod_pod" "cpu_instance" {
   support_public_ip    = true
   network_volume_id    = runpod_network_volume.storage.id
 
-  cpu_flavor_ids       = ["cpu3g"] # ["cpu5c"]
+  cpu_flavor_ids       = ["cpu5c"]
 
-  #vcpu_count           = 32
+  vcpu_count           = 32
   volume_in_gb         = 50
   container_disk_in_gb = 5
 
