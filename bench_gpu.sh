5bf68f95a3bcb43fdcdbc2c7fc1ab4895c7f9516
pkondratas <paulius.kondratas@gmail.com>
2026-05-11 14:43:43 +0300

    Remove cd step, adjust images

--- bench_gpu.sh
+++ bench_gpu.sh
@@ -4,6 +4,12 @@
 
 # max=0; while true; do vram=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | tr -d '\r'); if [ "$vram" -gt "$max" ]; then max=$vram; fi; echo -ne "Current: ${vram} MiB | Max: ${max} MiB\r"; sleep 1; done
 
+mkdir -p /workspace/cpu
+
+git clone --no-recurse-submodules https://github.com/pkondratas/zkvm_benchmarks.git
+cd zkvm_benchmarks
+git submodule update --init leanSig
+
 # SP1
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release -p sp1-host-gpu -- --n-signatures 1 --max-segment-limit 20 execute 2>&1 | tee -a sp1_1_sig.txt
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release -p sp1-host-gpu -- --n-signatures 1 --max-segment-limit 20 prove 2>&1 | tee -a sp1_1_sig.txt
