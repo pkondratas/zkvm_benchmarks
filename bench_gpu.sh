25b1c1ebaf7ae36400c47cd4f042efdf156d1c7b
pkondratas <120135235+pkondratas@users.noreply.github.com>
2026-05-13 16:03:27 +0300

    Gpu/test (#6)
    * Fix typo, testing commenting out
    
    * Uncomment, remove run terraform script

--- bench_gpu.sh
+++ bench_gpu.sh
@@ -4,7 +4,7 @@
 
 # max=0; while true; do vram=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | tr -d '\r'); if [ "$vram" -gt "$max" ]; then max=$vram; fi; echo -ne "Current: ${vram} MiB | Max: ${max} MiB\r"; sleep 1; done
 
-mkdir -p /workspace/cpu
+mkdir -p /workspace/gpu
 
 # SP1
 RUSTFLAGS="-C target-cpu=native" RUST_LOG=info cargo run --release -p sp1-host-gpu -- --n-signatures 1 --max-segment-limit 20 execute 2>&1 | tee -a /workspace/gpu/sp1_1_sig.txt
